1661f5536fc3632d1bf66c48ae7cfcbcb37548db
ongmk <ongmk.1997@gmail.com>
2020-04-05 16:55:50 +0800

    moved config, refactor set_status

--- COVID.py
+++ COVID.py
@@ -1,46 +1,38 @@
+from person import Community, COLOR_SCHEME, CONFIG
 from matplotlib import pyplot as plt
 from matplotlib import animation
-from person import Community
-
-population = 1000
-height = 1000
-
-# colors
-red = "#FF4C4C"
-green = "#34BF49"
-blue = "#0099E5"
-grey = "7B8D8E"
 
 with plt.style.context("dark_background"):
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10),
                                    gridspec_kw={'height_ratios': [3, 1]})
 ax1.set_aspect('equal')
 
-community = Community(population)
+community = Community()
 scat = ax1.scatter([p.pos[0] for p in community.people],
                    [p.pos[1] for p in community.people],
                    c=[p.color for p in community.people],
                    s=20)
-ax1.set_xlim(-height-100,height+100)
-ax1.set_ylim(-height-100,height+100)
+ax1.set_xlim(-CONFIG["HEIGHT"]-100,CONFIG["HEIGHT"]+100)
+ax1.set_ylim(-CONFIG["HEIGHT"]-100,CONFIG["HEIGHT"]+100)
 
 frames = [0]
-s_data = [[p.status for p in community.people].count("Susceptible")]
-i_data = [[p.status for p in community.people].count("Infected")]
+status_count = community.status_count()
+s_data = [status_count[0]]
+i_data = [status_count[1]]
 graph = ax2.stackplot(frames,s_data,i_data,labels=["S","I"],
-                      colors = [green,red])
-ax2.set_ylim(0, population)
+                      colors = [COLOR_SCHEME["S"],COLOR_SCHEME["I"]])
+ax2.set_ylim(0, CONFIG["POPULATION"])
 plots = [scat,graph]
 
 def animate(frame):
-    poss, colors = community.update()
+    poss, colors, status, status_count = community.update()
     scat.set_offsets(poss)
     scat.set_color(colors)
     frames.append(frame)
-    s_data.append([p.status for p in community.people].count("Susceptible"))
-    i_data.append([p.status for p in community.people].count("Infected"))
+    s_data.append(status_count[0])
+    i_data.append(status_count[1])
     graph = ax2.stackplot(frames,s_data,i_data,labels=["S","I"],
-                          colors = [green,red])
+                          colors = [COLOR_SCHEME["S"],COLOR_SCHEME["I"]])
     ax2.set_xlim(0,frame)
 
     return plots
--- person.py
+++ person.py
@@ -1,38 +1,40 @@
 import numpy as np
 
-height = 1000
+CONFIG = {
+    "HEIGHT" : 1000,
+    "DT" : 10,
+    "RADIUS" : 20.,
+    "INFECTED_P" : 0.05,
+    "SOCIAL_DISTANCING_FACTOR" : 0,
+    "POPULATION" : 1000
+}
 
 # colors
-red = "#FF4C4C"
-green = "#34BF49"
-blue = "#0099E5"
-grey = "7B8D8E"
+COLOR_SCHEME = {
+    "S" : "#34BF49",
+    "I" : "#FF4C4C",
+    "R" : "#0099E5",
+    "D" : "#7B8D8E"
+}
 
 class Person:
     def __init__(self):
         # parameters
-        self.status = 'Susceptible'
-        self.radius = 20.
-        self.infected_p = 0.05
-        self.social_dist_factor = 0
-        self.n_repulsion_points = 20
+        self.set_status("S")
+        self.radius = CONFIG["RADIUS"]
+        self.infected_p = CONFIG["INFECTED_P"]
+        self.social_dist_factor = CONFIG["SOCIAL_DISTANCING_FACTOR"]
         self.neighbours_pos=[]
         self.neighbours_status = []
         self.maxVel = 1
 
         # position parameters
-        self.pos = np.array((np.random.rand(2)-0.5)*2*height)
+        self.pos = np.array((np.random.rand(2)-0.5)*2*CONFIG["HEIGHT"])
         self.vel = (np.random.rand(2)+0.3)*12
         for i in range(2):
             if np.random.rand() < 0.5:
                 self.vel[i] = -self.vel[i]
         self.acc = np.zeros(2)
-
-        # plotting parameters
-        self.color = green
-        if np.random.rand() < 0.05:
-            self.color = red
-            self.status = "Infected"
 
     def social_dist(self):
         repulsion_force = np.zeros(2)
@@ -47,32 +49,39 @@ class Person:
                     repulsion_force -= self.social_dist_factor * displacement / (distance**3)
         self.acc = repulsion_force
 
-    def set_status(self):
+    def set_status(self, status):
+        assert status in ["S","I","R","D"]
+        self.status = {"S":"Susceptible",
+                       "I":"Infected",
+                       "R":"Recovered",
+                       "D":"Dead"}[status]
+        self.color = COLOR_SCHEME[status]
+
+    def check_infection(self):
         if len(self.neighbours_status) != 0:
             infected_neighbour = np.sum(self.neighbours_status=="Infected")
             if np.random.rand() < infected_neighbour * self.infected_p:
-                self.status = "Infected"
-                self.color = red
-        return self.color
+                self.set_status("I")
 
     def update(self):
-        dt = 10
+        dt = CONFIG["DT"]
         self.social_dist()
         self.vel += self.acc * dt
         if np.linalg.norm(self.vel) > self.maxVel:
             self.vel = self.maxVel * self.vel / np.linalg.norm(self.vel)
         self.pos += self.vel*dt
-        self.color = self.set_status()
-        if self.pos[0] > height or self.pos[0] < -height:
+        self.check_infection()
+        if self.pos[0] > CONFIG["HEIGHT"] or self.pos[0] < -CONFIG["HEIGHT"]:
             self.vel[0] = -self.vel[0]
-        if self.pos[1] > height or self.pos[1] < -height:
+        if self.pos[1] > CONFIG["HEIGHT"] or self.pos[1] < -CONFIG["HEIGHT"]:
             self.vel[1] = -self.vel[1]
 
 
 class Community:
-    def __init__(self,n):
-        self.population = n
-        self.people = [Person() for i in range(n)]
+    def __init__(self,population= CONFIG["POPULATION"]):
+        self.population = population
+        self.people = [Person() for i in range(population)]
+        [person.set_status("I") for person in self.people[:10]]
 
     def get_positions(self):
         return np.array([person.pos for person in self.people])
@@ -82,6 +91,13 @@ class Community:
 
     def get_people(self):
         return self.people
+
+    def status_count(self):
+        s_count = np.sum(self.get_status() == "Susceptible")
+        i_count = np.sum(self.get_status() == "Infected")
+        r_count = np.sum(self.get_status() == "Recovered")
+        d_count = np.sum(self.get_status() == "Dead")
+        return [s_count, i_count, r_count, s_count]
 
     def update(self):
         poss = []
@@ -98,4 +114,4 @@ class Community:
             person.update()
             poss.append(person.pos)
             colors.append(person.color)
-        return poss, colors
+        return poss, colors, status, self.status_count()
